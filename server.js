3320772563a6f6e325a5210a24baa446e9ada0cc
superdev <superdev0241002@gmail.com>
2022-12-02 05:35:57 +0900

    convert PostSQL

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const config = require("././config/config");
+
+const cors = require("cors");
+const db = require("./model/");
+const app = express();
+/**
+ *  Feature : This code allows you to access the server
+              In detail, 3001 port can fetch this server
+ *  Reason  : If you don't have cors, you will have an error owing to browser security
+ * 
+ */
+app.use(
+  cors({
+    origin: "*", //FrontEnd port : 3001
+  })
+);
+//end
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+/** Connect MySql database */
+// db.sequelize.sync()
+//   .then(() => {
+//     console.log("Synced db.");
+//   })
+//   .catch((err) => {
+//     console.log("Failed to sync db: " + err.message);
+//   });
+/**End */
+/////////////////////////////////////////////
+/**PostSQL database */
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+require("./routes/router")(app);
+
+app.listen(config.app.port);
